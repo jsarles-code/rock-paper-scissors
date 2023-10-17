431481a59881f17491de2cb75dc40d1d065e072a
Jack Sarles <Jack.Sarles@mcmcg.com>
2023-10-17 16:41:12 -0700

    Update RPS.js

--- RPS.js
+++ RPS.js
@@ -2,11 +2,56 @@ console.log("Hello, World!")
 
 function getComputerChoice() {
     let randInt = Math.floor(Math.random() * 3);
-    let array = ["Rock", "Scissors", "Paper"];
+    let array = ["rock", "paper", "scissors"];
     let computerchoice = array[randInt];
     console.log(computerchoice);
     return computerchoice;
-    
 }
 
-getComputerChoice()
+function playround(playerSelection, computerSelection) {
+    let playerSelection = prompt("Rock,Paper,Scissors?");
+    let computerSelection = getComputerChoice();
+     playerSelection = playerSelection.toLowerCase;
+
+    if (computerSelection == playerSelection) {
+        return "It's a draw"
+    }
+
+    else {
+        switch (playerSelection) {
+
+
+            case "rock":
+                if (computerSelection = "Paper") { return "Paper beats Rock , You lose" }
+                else {
+                    return "Rock beats Scissors You win!"
+                }
+                break;
+
+            case "paper":
+                if (computerSelection = "Scissors") { return "Scissors beats Rock , You lose" }
+                else {
+                    return "Rock beats Scissors You win!"
+
+                    break;
+    
+            case "scissors":
+                if (computerSelection = "Rock") { return "Rock Beats Scissors, You lose" }
+                else {
+                    return "Rock beats Scissors You win!"
+                }
+                break;
+
+        }
+
+    }
+
+
+}
+}
+
+
+
+console.log(getComputerChoice());
+
+console.log(playRound(playerSelection, computerSelection));
