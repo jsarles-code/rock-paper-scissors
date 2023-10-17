43d74560e8fe21076397a741e48ce1e34f168837
Jack Sarles <Jack.Sarles@mcmcg.com>
2023-10-17 15:28:38 -0700

    Update RPS.js

--- RPS.js
+++ RPS.js
@@ -4,7 +4,9 @@ function getComputerChoice() {
     let randInt = Math.floor(Math.random() * 3);
     let array = ["Rock", "Scissors", "Paper"];
     let computerchoice = array[randInt];
-    return computerchoice;
     console.log(computerchoice);
+    return computerchoice;
+    
 }
-getComputerChoice();
+
+getComputerChoice()
